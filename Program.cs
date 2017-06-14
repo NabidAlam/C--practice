38bb221b210fb88a4f568e96b582e6d6eae4bccd
Nabid <msa.nabid007@gmail.com>
2017-06-15 01:18:39 +0600

    Path

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,26 @@ namespace ConsoleApplication4
     {
         static void Main(string[] args)
         {
-            DateTime sd= new DateTime(2017,6,15);
-            Console.WriteLine("Starting date: {0}",sd);
-
-            DateTime ed = sd.AddDays(10);
-            Console.WriteLine("Ending date: {0}",ed);
-
-
-            if (ed > sd)
+            string p = @"c:\srip\sri.txt";
+            string p2 = @"c:\srip\sri";
+            string p3 = @"srip";
+            if (Path.HasExtension(p))
             {
-                Console.WriteLine("{0} occurs after {1}",sd,ed);
+                Console.WriteLine("{0} has an extension.", p);
             }
-
+            if (!Path.HasExtension(p2))
+            {
+                Console.WriteLine("{0} has no extension.", p2);
+            }
+            if (!Path.IsPathRooted(p3))
+            {
+                Console.WriteLine("The string {0} contains no root information.", p3);
+            }
+            Console.WriteLine("Location for Temporary Files : {0}", Path.GetTempPath());
+            Console.WriteLine("Full path of {0} is {1}.", p3, Path.GetFullPath(p3));
+            Console.WriteLine("File available for Use : {0} ", Path.GetTempFileName());
             Console.Read();
+ 
         }
     }
 }
